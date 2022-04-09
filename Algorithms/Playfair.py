1604eaa2d6046682887fa1753a9b010fc0649d0a
kcbojanowski <kbojanowski@student.agh.edu.pl>
2022-04-09 17:37:33 +0200

    Vigenere,Playfair added

---
+++ Algorithms/Playfair.py
@@ -0,0 +1,129 @@
+"""
+Encryption and Decryption of Playfair Cipher
+"""
+
+def create_matrix(key):
+    key = key.upper()
+    matrix = [[0 for col in range(5)] for row in range(5)]
+    alphabet = [chr(letter) for letter in range(65, 91)]
+    alphabet.remove('J')
+    used_letters = []
+    col = 0
+    row = 0
+
+    for letter in key:
+        if letter not in used_letters:
+            used_letters.append(letter)
+
+    for letter in alphabet:
+        if letter not in used_letters:
+            used_letters.append(letter)
+
+    nr = 0
+    for i in range(5):
+        for j in range(5):
+            matrix[i][j] = used_letters[nr]
+            nr += 1
+    return matrix
+
+
+def index_matrix(matrix, letter):
+    for i, j in enumerate(matrix):
+        if letter in j:
+            return i, j.index(letter)
+
+
+def Encrypt_Playfair(message, key):
+    encrypt_mess = []
+    matrix = create_matrix(key)
+    message = message.upper()
+
+# adding 'X' in case of two similar symbols next to each other
+    for i, j in zip(message, message[1:]):
+        if i == j:
+            message = message[:message.index(i)] + 'X' + message[message.index(i):]
+
+# adding 'Z' in case of odd number of letters in message
+    if not len(message) % 2:
+        message += 'Z'
+
+    for n in range(0, len(message)-1, 2):
+        l1 = list(index_matrix(matrix, message[n]))
+        l2 = list(index_matrix(matrix, message[n+1]))
+
+# Case no.1 - same column
+        if l1[1] == l2[1]:
+            if l1[0] + 1 == 5:
+                encrypt_mess.append(matrix[0][l1[1]])
+            else:
+                encrypt_mess.append(matrix[l1[0] + 1][l1[1]])
+
+            if l2[0] + 1 == 5:
+                encrypt_mess.append(matrix[0][l1[1]])
+            else:
+                encrypt_mess.append(matrix[l2[0] + 1][l2[1]])
+
+# Case no.2 - same row
+        elif l1[0] == l2[0]:
+            if l1[1] + 1 == 5:
+                encrypt_mess.append(matrix[l1[0]][0])
+            else:
+                encrypt_mess.append(matrix[l1[0]][l1[1] + 1])
+
+            if l2[1] + 1 == 5:
+                encrypt_mess.append(matrix[l2[0]][0])
+            else:
+                encrypt_mess.append(matrix[l2[0]][l2[1] + 1])
+
+# Case no.3 - Normal Playfair algorithm
+        else:
+            encrypt_mess.append(matrix[l1[0]][l2[1]])
+            encrypt_mess.append(matrix[l2[0]][l1[1]])
+
+    return "".join(encrypt_mess)
+
+
+def Decrypt_Playfair(message, key):
+    decrypt_mess = []
+    matrix = create_matrix(key)
+    message = message.upper()
+
+    for n in range(0, len(message) - 1, 2):
+        l1 = list(index_matrix(matrix, message[n]))
+        l2 = list(index_matrix(matrix, message[n + 1]))
+
+        # Case no.1 - same column
+        if l1[1] == l2[1]:
+            if l1[0] == 0:
+                decrypt_mess.append(matrix[4][l1[1]])
+            else:
+                decrypt_mess.append(matrix[l1[0] - 1][l1[1]])
+
+            if l2[0] == 0:
+                decrypt_mess.append(matrix[4][l1[1]])
+            else:
+                decrypt_mess.append(matrix[l2[0] - 1][l2[1]])
+
+        # Case no.2 - same row
+        elif l1[0] == l2[0]:
+            if l1[1] == 0:
+                decrypt_mess.append(matrix[l1[0]][4])
+            else:
+                decrypt_mess.append(matrix[l1[0]][l1[1] - 1])
+
+            if l2[1] == 0:
+                decrypt_mess.append(matrix[l2[0]][4])
+            else:
+                decrypt_mess.append(matrix[l2[0]][l2[1] - 1])
+
+        # Case no.3 - Normal Playfair algorithm
+        else:
+            decrypt_mess.append(matrix[l1[0]][l2[1]])
+            decrypt_mess.append(matrix[l2[0]][l1[1]])
+
+    return "".join(decrypt_mess)
+
+
+if __name__ == '__main__':
+    print(Encrypt_Playfair('qwertyuiop', 'kacper'))
+    print(Decrypt_Playfair('wakgypqntk', 'kacper'))
---
+++ Algorithms/Vigenere.py
@@ -0,0 +1,46 @@
+"""
+Encryption and Decryption of Vigenere Cipher
+"""
+
+
+def generateKey(string, key):
+    key = list(key)
+    string = string.replace(" ", "")
+
+    if len(string) == len(key):
+        return key
+    else:
+        for i in range(len(string) - len(key)):
+            key.append(key[i % len(key)])
+
+    key = "".join(key).upper()
+    return key
+
+
+def Encrypt_Vigenere(message, keyword):
+    encrypt_mess = ""
+    message = message.upper()
+    key = generateKey(message, keyword)
+
+    for i in range(len(message)):
+        en = (ord(message[i]) + ord(key[i])) % 26
+        encrypt_mess = encrypt_mess + chr(en + 65)
+    return encrypt_mess
+
+
+def Decrypt_Vigenere(message, keyword):
+    decrypt_mess = ""
+    message = message.upper()
+    key = generateKey(message, keyword)
+
+    for i in range(len(message)):
+        dec = (ord(message[i]) - ord(key[i]) + 26) % 26
+        decrypt_mess = decrypt_mess + chr(dec + 65)
+    return decrypt_mess
+
+
+if __name__ == '__main__':
+    mess = 'QWERTYUIOP'
+    key = 'crypti'
+    print(Encrypt_Vigenere(mess, key))
+    print(Decrypt_Vigenere(mess, key))
